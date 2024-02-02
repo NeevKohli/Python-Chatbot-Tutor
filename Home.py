0b000a236dd8dd22ebb20197de5662613fab4b09
NeevKohli <zceenko@ucl.ac.uk>
2024-02-02 20:32:49 +0000

    Update

--- Home.py
+++ Home.py
@@ -7,31 +7,4 @@ import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 
-hide_pages(
-    [
-        Page(r"pages/Welcome.py", "Home", ":house:"),
-        Page(r"pages/Info.py", "Important Information", ":octagonal_sign:"),
-        Page(r"pages/Chatbot.py", "PyBot", ":snake:")
-    ]
-)
-
-with open('config.yml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days']
-)
-
-authenticator.login()
-
-if st.session_state["authentication_status"]:
-    switch_page("Welcome")
-
-elif st.session_state["authentication_status"] is False:
-    st.error('This UCL email address has not been granted access to this application.')
-
-elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your UCL email address.')
+switch_page("Login")
---
+++ pages/Login.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from streamlit_chat import message
+from streamlit_extras.switch_page_button import switch_page
+from st_pages import Page, show_pages, hide_pages, add_page_title
+from PIL import Image
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+hide_pages(
+    [
+        Page(r"pages/Welcome.py", "Home", ":house:"),
+        Page(r"pages/Info.py", "Important Information", ":octagonal_sign:"),
+        Page(r"pages/Chatbot.py", "PyBot", ":snake:")
+    ]
+)
+
+with open('config.yml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
+
+authenticator.login()
+
+if st.session_state["authentication_status"]:
+    switch_page("Welcome")
+
+elif st.session_state["authentication_status"] is False:
+    st.error('This UCL email address has not been granted access to this application.')
+
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your UCL email address.')
