0e3c97beb555bc484650d4cfce32f1e703643c13
NeevKohli <zceenko@ucl.ac.uk>
2024-02-02 17:56:14 +0000

    Update

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ import streamlit_authenticator as stauth
 #Add Alejandra's, Arnas', Iason's and the 5 students email later (10 testers in total of which 2 are white-box and 8 are black-box)
 emails = ['zceenko@ucl.ac.uk','zceemc0@ucl.ac.uk']
 
-hashed_emails = stauth.hasher(emails).generate()
+hashed_emails = stauth.Hasher(emails).generate()
 
 authenticator = stauth.authenticate(hashed_emails,
     'some_cookie_name','some_signature_key',cookie_expiry_days=30)
