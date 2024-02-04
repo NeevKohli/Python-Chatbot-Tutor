d7d23b1f70cdc47d6049e3e05abdeffe8cc90d90
NGC4414 <claramarchese86@gmail.com>
2024-02-04 23:22:29 +0000

    i

--- pages/Login.py
+++ pages/Login.py
@@ -38,7 +38,7 @@ authenticator = stauth.Authenticate(
 )
 
 #authenticator.login()
-authentication_status = authenticator.login('Login')
+authentication_status = authenticator.login()
 
 if st.session_state["authentication_status"]:
     st.write(f'Welcome *{st.session_state["name"]}*.')
